bc5de8e6db316f7290dd5f0cd9b8cbd389933423
Prince Mensah <princemensaheyram@gmail.com>
2023-07-04 21:00:05 +0000

    changes made to pop_listint file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,51 +1,30 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node
- * at a given position.
+ * pop_listint - deletes the head node of
+ * a linked list
  * @head: head of a list.
- * @idx: index of the list where the new node is
- * added.
- * @n: integer element.
  *
- * Return: the address of the new node, or NULL if it
- * failed.
+ * Return: head node's data.
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int pop_listint(listint_t **head)
 {
-	unsigned int i;
-	listint_t *new;
+	int hnode;
 	listint_t *h;
+	listint_t *curr;
 
-	h = *head;
+	if (*head == NULL)
+		return (0);
 
-	if (idx != 0)
-	{
-		for (i = 0; i < idx - 1 && h != NULL; i++)
-		{
-			h = h->next;
-		}
-	}
+	curr = *head;
 
-	if (h == NULL && idx != 0)
-		return (NULL);
+	hnode = curr->n;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+	h = curr->next;
 
-	new->n = n;
+	free(curr);
 
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		new->next = h->next;
-		h->next = new;
-	}
+	*head = h;
 
-	return (new);
+	return (hnode);
 }
